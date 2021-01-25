ba3872c628065e3282ec5f91aa4a272e0d26be66
cliffgor <clierozigy@gmail.com>
2021-01-25 20:57:13 +0300

    added axios to fetch data from the crypro api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect, useState } from 'react'
+
+import axios from 'axios'
+
 function App() {
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data)
+      
+    }).catch(error => alert('You have an error' + error))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+      <h1>Crypto Tracker App</h1>
+       <div className='coin-search'>
+         <h1 className='coin-text'>Search currency</h1>
+         <form>
+           <input type='text' placeholder='Search'
+           className='coin-input'/>
+         </form>
+       </div>
+       
     </div>
+
+
   );
 }
 
---
+++ src/Coin.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Coin = () => {
+    return (
+        <div className='coin-container'>
+            <div className='coin-row'>
+                <div className='coin'>
+                    <img src={Image} alt="crypto"/>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Coin
